62fc7ce6ac5062cb39ee68ab8a3e29af2d3ecec4
DanielDias-hub <85328154+DanielDias-hub@users.noreply.github.com>
2021-07-05 08:22:54 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m21.mk
+    $(LOCAL_DIR)/bootleg_m21.mk
 
 COMMON_LUNCH_CHOICES := \
-     lineage_m21-userdebug \
-     lineage_m21-eng \
-     lineage_m21-user
+     bootleg_m21-userdebug \
+     bootleg_m21-eng \
+     bootleg_m21-user
