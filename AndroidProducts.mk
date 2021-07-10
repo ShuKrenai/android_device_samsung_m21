a1965c45a2097cb58cb0b4fc358153e87f45966c
DanielDias-hub <85328154+DanielDias-hub@users.noreply.github.com>
2021-07-10 20:45:22 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m21.mk
+    $(LOCAL_DIR)/xtended_m21.mk
 
 COMMON_LUNCH_CHOICES := \
-     lineage_m21-userdebug \
-     lineage_m21-eng \
-     lineage_m21-user
+     xtended_m21-userdebug \
+     xtended_m21-eng \
+     xtended_m21-user
